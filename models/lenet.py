dffe97c8c02f16abf42936cc3d1c43a6729d9c1c
Luis Muñoz-González <l.munoz@imperial.ac.uk>
2023-05-22 17:28:01 +0100

    Add files via upload

---
+++ models/lenet.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+#LeNet architecture for images with 3 channels and 32x32 pixels
+class LeNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, kernel_size = 5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size = 5)
+        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, num_classes)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+#LeNet architecture with a single channel and 28x28 (e.g. for MNIST o Fashion-MNIST)
+class LeNet1c(nn.Module):
+    def __init__(self):
+        super(LeNet1c, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size = 5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size = 5)
+        self.fc1   = nn.Linear(16*4*4, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+
